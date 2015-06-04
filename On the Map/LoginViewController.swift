6e3f3c84c4fcf50c56a07908f053546851c75f23
Troy Tobin <ttobin@Troys-MacBook-Pro.local>
2015-06-04 23:57:40 +0930

    Crude udacity login implemented.

---
+++ On the Map/LoginViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Troy Tobin on 3/06/2015.
+//  Copyright (c) 2015 ttobin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate{
+
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    /// Set this to be the delegate for the login text fields
+    emailTextField.delegate = self
+    passwordTextField.delegate = self
+  }
+  
+  @IBAction func loginButtonPressed(sender: AnyObject) {
+    println("Login")
+    let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+    request.HTTPMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.HTTPBody = "{\"udacity\": {\"username\": \"\(emailTextField.text)\", \"password\": \"\(passwordTextField.text)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+    let session = NSURLSession.sharedSession()
+    println("do task")
+    let task = session.dataTaskWithRequest(request) { data, response, error in
+      if (error != nil) {
+        println("Error login to udacity")
+        return
+      }
+      var parsingError: NSError? = nil
+      
+      let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+      
+      let parsedResult: AnyObject? = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parsingError)
+      
+      println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+      if let error = parsingError {
+        println("error parsing data")
+        return
+      } else if let account = parsedResult?.valueForKey("account") as? NSDictionary? {
+        if let registered = account?.valueForKey("registered") as? Bool {
+          if (registered == true) {
+            let key = account?.valueForKey("key") as? String
+            println(key)
+            dispatch_async(dispatch_get_main_queue(), {
+              let controller = self.storyboard!.instantiateViewControllerWithIdentifier("OnTheMapNavigationController") as! UINavigationController
+              self.presentViewController(controller, animated: true, completion: nil)
+            })
+          }
+        }
+        
+      } else {
+        println("not much happening here")
+      }
+      
+    }
+    task.resume()
+  }
+  
+  func textFieldDidBeginEditing(textField: UITextField)
+  {
+    textField.text = ""
+    if(textField == passwordTextField){
+      passwordTextField.secureTextEntry = true;
+    }
+  }
+
+}
+
