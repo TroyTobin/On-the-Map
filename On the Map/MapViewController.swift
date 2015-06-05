24c799316e7e974caee42eb5c3a8faf94481ebc8
Troy Tobin <ttobin@Troys-MacBook-Pro.local>
2015-06-05 20:48:41 +0930

    Added MapKit and Map view when logged in.

---
+++ On the Map/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  On the Map
+//
+//  Created by Troy Tobin on 5/06/2015.
+//  Copyright (c) 2015 ttobin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
